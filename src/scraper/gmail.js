3f2afd17e2281f95434c7edae6e3ea6a0a8fd69d
Frédéric Bonnet <fredericbonnet@free.fr>
2016-08-26 16:15:28 +0200

    Added profile info and subject lines

--- src/scraper/gmail.js
+++ src/scraper/gmail.js
@@ -33,11 +33,20 @@
     
     // Gmail settings.
     var CLIENT_ID = "612453794408-e72qm8av2gi1sa7s1tq36o2aio1ksfqa.apps.googleusercontent.com";
-    var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
+    var SCOPES = 'profile,https://www.googleapis.com/auth/userinfo.profile,https://www.googleapis.com/auth/gmail.readonly'.split(',');
     
     // Load the SDK asynchronously
     window.gmAsyncInit = function() {
-        console.log("Google API loaded");
+        gapi.client.load('plus', 'v1').then(
+            function() {
+                gapi.client.load('gmail', 'v1').then(function() {
+                        console.log("Gmail API loaded");
+                    },
+                    console.error
+                );
+            },
+            console.error
+        )
     };
     (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
@@ -107,6 +116,64 @@
         );
     };
     
+    /**
+     * Get messages from Gmail.
+     *
+     *  @param callback     Function called with results.
+     */
+    var getMessages = function(callback) {
+        gapi.client.gmail.users.messages.list({userId: 'me'}).then(
+            function(response) {
+                // Get all messages in batch.
+                var messages = response.result.messages;
+                var batch = gapi.client.newBatch();
+                for (var i = 0; i < messages.length; i++) {
+                    var message = messages[i];
+                    batch.add(gapi.client.gmail.users.messages.get({userId: 'me', id: message.id}));
+                }
+                batch.then(
+                    function(response) {
+                        var info = {messages: []};
+                        for (var id in response.result) {
+                            var result = response.result[id].result;
+                            if (result.error) {
+                                // Ignore.
+                            } else {
+                                var message = {};
+                                
+                                // Get subject.
+                                var headers = result.payload.headers;
+                                for (var i = 0; i < headers.length; i++) {
+                                    var header = headers[i];
+                                    if (header.name.toLowerCase() == 'subject') {
+                                        message.subject = header.value;
+                                    }
+                                }
+                                
+                                // Get body TODO
+                                
+                                // Get images TODO
+                                
+                                info.messages.push(message);
+                            }
+                        }
+                        
+                        // Done!
+                        callback(info);
+                    },
+                    function(reason) {
+                        // Pass to callback.
+                        callback({error: reason});
+                    }
+                );                
+            },
+            function(reason) {
+                // Pass to callback.
+                callback({error: reason});
+            }
+        );
+    }
+    
     
     /*
      *
@@ -117,27 +184,87 @@
     /**
      * Fetch & scrape Gmail content. We get the following info:
      *
-     *  - Name, headline title, summary, .
-     *  - Number of connections.
-     *  - Locations, positions (current and past)
-     *  - Profile images.
+     *  - Subject line.
+     *  - Textual body TODO
+     *  - Embedded images TODO
      *
      *  @param callback     Function called with content info.
      */ 
     provider.fetch = function(callback) {
+        var info = {success: false};
         authorize(function(response) {
             if (response && !response.error) {
-                //TODO
-                console.error(response);
-                var info = {};
-                info.success = false;
-                info.error = "TODO";
-                callback(info);
+                // Get profile info and message list.
+                var batch = gapi.client.newBatch();
+                var peopleId = batch.add(gapi.client.plus.people.get({userId: 'me'}));
+                var getProfileId = batch.add(gapi.client.gmail.users.getProfile({userId: 'me'}));
+                batch.then(
+                    function(response) {
+                        var people = response.result[peopleId].result;
+                        var profile = response.result[getProfileId].result;
+                        
+                        // Main info.
+                        info.id = profile.emailAddress||'';
+                        info.url = 'mailto:'+profile.emailAddress;
+                        info.label = people.displayName||'';
+                        
+                        // Profile image.
+                        info.images = [];
+                        if (people.image) {
+                            info.images.push(people.image.url);
+                        }
+                    
+                        // Sentences.
+                        info.sentences = [];
+                        
+                        // - Title = name.
+                        info.sentences.push(people.displayName||'');
+                        
+                        // - Subtitle = email address.
+                        info.sentences.push(profile.emailAddress||'');
+                        
+                        // - Price = number of messages.
+                        info.sentences.push(profile.messagesTotal.toString());
+                        
+                        // Message subjects/body/images.
+                        getMessages(function(infoMessages) {
+                            if (infoMessages.messages) {
+                                for (var i = 0; i < infoMessages.messages.length; i++) {
+                                    var message = infoMessages.messages[i];
+                                    if (message.subject) {
+                                        info.sentences.push(message.subject);
+                                    }
+                                    if (message.body) {
+                                        var sentences = splitSentences(message.body);
+                                        for (var j = 0; j < sentences.length; j++) {
+                                            info.sentences.push(sentences[j]);
+                                        }
+                                    }
+                                    if (message.images) {
+                                        for (var j = 0; j < message.images.length; j++) {
+                                            info.images.push(message.images[j]);
+                                        }
+                                    }
+                                }
+                        
+                                // Done!
+                                info.success = true;
+                                callback(info);
+                            } else {
+                                info.error = "Can't get messages";
+                                callback(info);
+                            }
+                        });
+                    },
+                    function(reason) {
+                        // Batch failed, can't do much about that.
+                        info.error = "API call failed";
+                        callback(info);
+                    }
+                );
             } else {
                 // Can't issue API calls.
                 console.error(response);
-                var info = {};
-                info.success = false;
                 switch (response.error) {
                     case "closed":
                     case "denied":  info.error = "Authorization denied";    break;
